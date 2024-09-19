813afbc41bc247198e456d6ad5c06b2bd6180e9b
Dinidu Sri Nimesha <131584114+dinidusrinimesha@users.noreply.github.com>
2024-09-19 20:50:10 +0530

    student-bio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import StudentBio from "./components/StudentBio";
+
+import styles from './App.css';
+
+function App() {
+  return (
+    <body>
+      <div className="App">
+        <StudentBio />
+      </div>
+    </body> 
+  );
+}
+
+export default App;
+
